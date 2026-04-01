c1d8a6e68b200d45b014acf723ec6b6520254cad
Alhamd Khan <alhamdkhan016@gmail.com>
2026-04-01 16:19:27 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./App.css"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from "./src/pages/Home";
+import Appointment from "./src/pages/Appointment";
+import AboutUs from "./src/pages/AboutUs";
+import Register from './src/pages/Register';
+import Login from './src/pages/Login';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const App = () => {
+    return (
+      <>
+      <Router>
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/appointment' element={<Appointment/>}/>
+            <Route path='/about' element={<AboutUs/>}/>
+            <Route path='/register' element={<Register/>}/>
+            <Route path='/login' element={<Login/>}/>
+        </Routes>
+        <ToastContainer position="top-center"/>
+      </Router>
+      </>
+    )
+}
+
+export default App;
